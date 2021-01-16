f7e3657d73f224d0a2ffd7745bb43096ad7ed3d7
Waqas Ali <3605691+waqasali47@users.noreply.github.com>
2021-01-16 15:10:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,9 @@ OLED.init(128, 64)
 basic.forever(function () {
     temp = smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P1)
     OLED.clear()
-    OLED.writeString("temperatur")
+    OLED.writeString("temperature ")
     OLED.writeNum(temp)
+    OLED.newLine()
     OLED.writeString("abdullah")
     if (temp > 25) {
         pins.digitalWritePin(DigitalPin.P2, 1)
